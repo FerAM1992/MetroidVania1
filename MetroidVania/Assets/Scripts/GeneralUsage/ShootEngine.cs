f24a5d0b4b2a67082c319ce6434d966f48206845
Fernando <fer.alm.med@hotmail.com>
2019-04-21 14:37:13 -0500

    Added shot capability with direction and force

---
+++ MetroidVania/Assets/Scripts/GeneralUsage/ShootEngine.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootEngine : MonoBehaviour
+{
+    [Header("Shoot attributes")]
+    [SerializeField] GameObject shootPrefab;
+    [SerializeField] float shootSpeed = 10f;
+
+    Coroutine firingCoroutine;
+
+    GameObject aimingArea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aimingArea = GameObject.Find("PlayerAim");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shoot();
+    }
+
+
+    private void shoot()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            firingCoroutine = StartCoroutine(FireContinuosly());
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(firingCoroutine);
+        }
+    }
+
+
+
+    IEnumerator FireContinuosly()
+    {
+        Vector2 playerPosition = new Vector2(transform.position.x, transform.position.y);
+     Vector2 targetPosition = (TargetEngine.mousePosition - playerPosition).normalized;
+     GameObject laser = Instantiate(shootPrefab,new Vector3(transform.position.x, transform.position.y + .3f, transform.position.z)
+                        , Quaternion.identity) as GameObject;
+
+        laser.GetComponent<Rigidbody2D>().velocity += targetPosition* shootSpeed * Time.deltaTime;
+
+       
+     yield return new WaitForSeconds(0.5f);
+
+    }
+
+    
+
+}
---
+++ MetroidVania/Assets/Scripts/Player/TargetEngine.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetEngine : MonoBehaviour {
+
+    //configuration parameters
+
+    [SerializeField] float aimingRadious = 3f;
+    [SerializeField] float aimingAreaYOffset = 1.75f;
+    public static Vector2 mousePosition;
+    Transform aimingArea;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = GetTargetPosition();
+    }
+
+    public Vector2 GetTargetPosition()
+    {
+        aimingArea = GameObject.Find("Player").transform;
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 aimingAreaCenter = aimingArea.localPosition;
+        aimingAreaCenter += new Vector2(0, aimingAreaYOffset);
+        Vector2 v = mousePosition - aimingAreaCenter;
+        v = Vector2.ClampMagnitude(v, aimingRadious);
+        mousePosition = aimingAreaCenter + v;
+        return mousePosition;
+    }
+}
